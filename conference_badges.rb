c0947588f4e91eab1b2330dca9d72893f86d3820
Devin Arnold <dbarnold321@gmail.com>
2019-05-11 12:58:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def badge_maker (name)
+  return "Hello, my name is #{name}."
+end
+
+def 
+
+def assign_rooms (x)
+  return "Hello, #{name}! You'll be assigned to room #{rooms}!"
+end
