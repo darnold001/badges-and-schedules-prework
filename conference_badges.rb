4af3d7bf61f1e8c67c3817799cf397c70ca1f285
Devin Arnold <dbarnold321@gmail.com>
2019-05-11 12:58:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,12 @@ def badge_maker (name)
   return "Hello, my name is #{name}."
 end
 
-def 
+def batch_badge_creator (person)
+  person.each do |n|
+    speaker = " Hello #{n}!"
+    puts speaker
+  end
+
 
 def assign_rooms (x)
   return "Hello, #{name}! You'll be assigned to room #{rooms}!"
